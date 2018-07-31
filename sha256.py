438e41023452c53457ede957d9ba6cd13a9f4205
Stephan Brauer <stephan@ls42.de>
2018-07-31 23:11:19 +0200

    Add verbose mode (-v True required unforunately)

--- sha256.py
+++ sha256.py
@@ -5,12 +5,15 @@
 from hashlib import sha256
 import argparse
 import os
+import sys
 
 parser = argparse.ArgumentParser('Verify sha256 hash of file')
 parser.add_argument('-f', '--filename', type=str,
                     help='File that needs hashing', required=True)
 parser.add_argument('-s', '--filesum', type=str,
                     help='Correct hashsum of that file', required=True)
+parser.add_argument('-v', '--verbose', type=bool,
+                    help='Print actual and expected hashsums')
 args = parser.parse_args()
 
 
@@ -20,9 +23,15 @@ class tcolors:
     NORMAL = '\033[0m'
 
 
-file_obj = open(args.filename, 'rb')
+try:
+    file_obj = open(args.filename, 'rb')
+except FileNotFoundError as e:
+    print(str(e))
+    sys.exit()
 real_sum = sha256(file_obj.read()).hexdigest()
+if args.verbose:
+    print('actual: {}\nexpected: {}'.format(real_sum, args.filesum))
 if real_sum == args.filesum:
-    print('{}OK.{}'.format(tcolors.OK, tcolors.NORMAL))
+    print('{}OK{}'.format(tcolors.OK, tcolors.NORMAL))
 else:
     print('{}NOK{}'.format(tcolors.FAIL, tcolors.NORMAL))
