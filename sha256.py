56c92595ccf1b577e67cc533efc4cfbb491aa0f9
Stephan Brauer <stephan@ls42.de>
2018-08-01 13:06:59 +0200

    Make -v work as expected.

--- sha256.py
+++ sha256.py
@@ -12,8 +12,8 @@ parser.add_argument('-f', '--filename', type=str,
                     help='File that needs hashing', required=True)
 parser.add_argument('-s', '--filesum', type=str,
                     help='Correct hashsum of that file', required=True)
-parser.add_argument('-v', '--verbose', type=bool,
-                    help='Print actual and expected hashsums')
+parser.add_argument('-v', '--verbose',
+                    help='Print actual and expected hashsums', action='store_true')
 args = parser.parse_args()
 
 
@@ -30,7 +30,7 @@ except FileNotFoundError as e:
     sys.exit()
 real_sum = sha256(file_obj.read()).hexdigest()
 if args.verbose:
-    print('actual: {}\nexpected: {}'.format(real_sum, args.filesum))
+    print('actual:   {}\nexpected: {}'.format(real_sum, args.filesum))
 if real_sum == args.filesum:
     print('{}OK{}'.format(tcolors.OK, tcolors.NORMAL))
 else:
